88cc1adbb508eb97e3d60958e4c55f474ff07c19
CE-sw <91242806+Tjdnjs@users.noreply.github.com>
2022-05-12 13:39:49 +0900

    Add files via upload

---
+++ 1419.py
@@ -0,0 +1,11 @@
+# 등차수열 첫 수 x 차이 d k번째 항까지 나열
+# x, x+d, ,,,,
+# left 보다 크거나 같고, right 보다 작거나 같은 정수
+# 몇 개가 등차수열의 앞 k항의 합으로 표현되는 수인 지
+
+import sys
+input = sys.stdin.readline
+
+left = int(input())
+right = int(input())
+k = int(input())
